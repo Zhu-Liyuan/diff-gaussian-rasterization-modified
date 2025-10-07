13d76490f6219398742cb7c17b1211af68500822
Zhu-Liyuan <Zhu-Liyuan@outlook.com>
2025-10-06 23:19:04 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-#
-# Copyright (C) 2023, Inria
-# GRAPHDECO research group, https://team.inria.fr/graphdeco
-# All rights reserved.
-#
-# This software is free for non-commercial, research and evaluation use 
-# under the terms of the LICENSE.md file.
-#
-# For inquiries contact  george.drettakis@inria.fr
-#
-
+# setup.py - minimal, H200 (compute 10.0 / sm_100) specific
 from setuptools import setup
 from torch.utils.cpp_extension import CUDAExtension, BuildExtension
 import os
-os.path.dirname(os.path.abspath(__file__))
+
+here = os.path.dirname(os.path.abspath(__file__))
+glm_include = "-I" + os.path.join(here, "third_party", "glm")
+
+# NVCC gencode flags for H200 (compute 10.0 -> 100)
+nvcc_gencode_flags = [
+    "-gencode=arch=compute_100,code=sm_100",
+    "-gencode=arch=compute_100,code=compute_100",  # PTX fallback
+]
+
+extra_nvcc_args = ["--extended-lambda"] + nvcc_gencode_flags + [glm_include]
 
 setup(
     name="diff_gaussian_rasterization",
@@ -21,14 +21,14 @@ setup(
         CUDAExtension(
             name="diff_gaussian_rasterization._C",
             sources=[
-            "cuda_rasterizer/rasterizer_impl.cu",
-            "cuda_rasterizer/forward.cu",
-            "cuda_rasterizer/backward.cu",
-            "rasterize_points.cu",
-            "ext.cpp"],
-            extra_compile_args={"nvcc": ["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "third_party/glm/")]})
-        ],
-    cmdclass={
-        'build_ext': BuildExtension
-    }
+                "cuda_rasterizer/rasterizer_impl.cu",
+                "cuda_rasterizer/forward.cu",
+                "cuda_rasterizer/backward.cu",
+                "rasterize_points.cu",
+                "ext.cpp"
+            ],
+            extra_compile_args={"nvcc": extra_nvcc_args}
+        )
+    ],
+    cmdclass={'build_ext': BuildExtension}
 )
